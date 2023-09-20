933f00bab2e23e0664c9df5d59a36c190eb54fe5
xuanan92 <nguyentruongxuananhn@gmail.com>
2023-09-20 23:42:05 +0700

    i

--- countdown.lua
+++ countdown.lua
@@ -1,70 +1,40 @@
 -- countdown.lua
 
-local notify = require("notify")
 local M = {}
 
 function M.countdown(seconds)
-	local countdown_job_id
 	local remaining = seconds
 
-	-- Function to update the countdown notification
-	local function update_notification()
-		notify("Countdown: " .. tostring(remaining) .. " seconds remaining", {
-			title = "Countdown",
-			timeout = 0,
-		})
-	end
+	local function display_message()
+		vim.cmd("vertical belowright new")
+		vim.cmd("setlocal buftype=nofile")
+		vim.cmd("setlocal bufhidden=wipe")
+		vim.cmd("setlocal noswapfile")
+		vim.cmd("setlocal nowrap")
+		vim.cmd("setlocal nolist")
+		vim.cmd("setlocal nonumber")
 
-	-- Function to handle the terminal job exit
-	local function on_exit(job_id, _, _)
-		if job_id == countdown_job_id then
-			countdown_job_id = nil
-			notify("Countdown: Time is up!", {
-				title = "Countdown",
-				timeout = 0,
-			})
+		for i = remaining, 0, -1 do
+			vim.api.nvim_buf_set_lines(0, -1, -1, false, { "Countdown: " .. i .. " seconds remaining" })
+			vim.wait(1000, function()
+				return vim.api.nvim_buf_line_count(0) == i + 1
+			end)
 		end
+
+		vim.cmd("q")
 	end
 
-	-- Start the countdown job in a new terminal window
-	local function start_countdown_terminal()
-		vim.cmd("terminal")
-		vim.cmd("startinsert")
-		countdown_job_id = vim.fn.termopen({
-			"sh",
-			"-c",
-			[[for ((s=]] .. seconds .. [[; s>=0; s--)); do
-          printf "Countdown: %02d seconds remaining\n" $s
-          sleep 1
-      done]],
-		})
-		vim.fn.termwait(countdown_job_id)
+	vim.schedule(display_message)
+end
+
+-- Function to be called from Neovim command-line
+function countdown(args)
+	local seconds = tonumber(args)
+	if seconds then
+		M.countdown(seconds)
+	else
+		print("Invalid argument. Please provide the countdown duration in seconds.")
 	end
-
-	-- Start the countdown
-	local function start_countdown()
-		update_notification()
-		vim.defer_fn(start_countdown_terminal, 0)
-	end
-
-	-- Function to be called from Neovim command-line
-	function countdown(args)
-		local seconds = tonumber(args)
-		if seconds then
-			remaining = seconds
-			start_countdown()
-		else
-			print("Invalid argument. Please provide the countdown duration in seconds.")
-		end
-	end
-
-	-- Setup the on_exit autocommand to handle terminal job exit
-	vim.cmd([[
-    autocmd! CountdownTerminalExit
-    autocmd TermClose * call v:lua.require'countdown'.on_exit(v:eventjobid, v:eventstatus, v:eventretval)
-  ]])
-
-	return M
 end
 
 return M
