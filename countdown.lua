fdaee8a18619262944cb435c6aa5ae2747e19b67
xuanan92 <nguyentruongxuananhn@gmail.com>
2023-09-20 23:47:03 +0700

    u

--- countdown.lua
+++ countdown.lua
@@ -6,22 +6,39 @@ function M.countdown(seconds)
 	local remaining = seconds
 
 	local function display_message()
-		vim.cmd("vertical belowright new")
-		vim.cmd("setlocal buftype=nofile")
-		vim.cmd("setlocal bufhidden=wipe")
-		vim.cmd("setlocal noswapfile")
-		vim.cmd("setlocal nowrap")
-		vim.cmd("setlocal nolist")
-		vim.cmd("setlocal nonumber")
+		local term_width = 50
+		local term_height = 1
 
+		-- Calculate the floating terminal position
+		local term_row = vim.o.lines - term_height - 1
+		local term_col = vim.o.columns - term_width
+
+		-- Create the floating terminal
+		local term_buf = vim.api.nvim_create_buf(false, true)
+		local term_win = vim.api.nvim_open_win(term_buf, true, {
+			relative = "editor",
+			row = term_row,
+			col = term_col,
+			width = term_width,
+			height = term_height,
+			style = "minimal",
+			border = "single",
+		})
+
+		-- Set terminal buffer options
+		vim.api.nvim_buf_set_option(term_buf, "buftype", "terminal")
+		vim.api.nvim_buf_set_option(term_buf, "bufhidden", "hide")
+		vim.api.nvim_buf_set_option(term_buf, "swapfile", false)
+		vim.api.nvim_buf_set_option(term_buf, "filetype", "terminal")
+
+		-- Start the countdown
 		for i = remaining, 0, -1 do
-			vim.api.nvim_buf_set_lines(0, -1, -1, false, { "Countdown: " .. i .. " seconds remaining" })
-			vim.wait(1000, function()
-				return vim.api.nvim_buf_line_count(0) == i + 1
-			end)
+			vim.fn.termopen("echo Countdown: " .. i .. " seconds remaining")
+			vim.wait(1000)
 		end
 
-		vim.cmd("q")
+		-- Close the terminal window
+		vim.api.nvim_win_close(term_win, true)
 	end
 
 	vim.schedule(display_message)
