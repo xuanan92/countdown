ad9139cbb52242ecfa15bff9ba5b75a7cbad3d14
xuanan92 <nguyentruongxuananhn@gmail.com>
2023-09-21 00:02:33 +0700

    u

--- countdown.lua
+++ countdown.lua
@@ -1,54 +1,39 @@
--- countdown.lua
+local function open_float_terminal()
+	-- Set the terminal dimensions
+	local width = 40
+	local height = 1
 
-local M = {}
+	-- Calculate the terminal window position
+	local col = math.floor((vim.o.columns - width) / 2)
+	local row = math.floor((vim.o.lines - height) / 2)
 
-function M.countdown(seconds)
-	local remaining = seconds
+	-- Open the terminal in a floating window
+	vim.cmd(string.format("terminal ++curwin ++rows=%d ++cols=%d ++row=%d ++col=%d", height, width, row, col))
+end
 
-	local function display_message()
-		local term_width = 50
-		local term_height = 1
-
-		-- Calculate the floating terminal position
-		local term_row = vim.o.lines - term_height - 1
-		local term_col = vim.o.columns - term_width
-
-		-- Open a new terminal window
-		vim.cmd("botright vsplit term://bash")
-
-		-- Resize the terminal window
-		vim.cmd(term_height .. "wincmd _")
-		vim.cmd(term_width .. "wincmd |")
-
-		-- Move the terminal window to the desired position
-		vim.cmd("wincmd J")
-		vim.cmd(term_row .. "wincmd _")
-		vim.cmd(term_col .. "wincmd |")
-
-		-- Start the countdown
-		for i = remaining, 0, -1 do
-			vim.fn.termopen("echo Countdown: " .. i .. " seconds remaining")
-
-			-- Allow editing the text while counting down
-			vim.cmd("startinsert")
-			vim.cmd("autocmd InsertLeave <buffer> stopinsert")
-
-			-- Wait for 1 second
-			vim.wait(1000)
-		end
-
-		-- Close the terminal window
-		vim.cmd("q!")
+local function start_countdown(time)
+	-- Check if the time is a valid number
+	local countdown_time = tonumber(time)
+	if countdown_time == nil then
+		print("Invalid countdown time")
+		return
 	end
 
-	vim.schedule(display_message)
+	-- Start the countdown
+	for i = countdown_time, 0, -1 do
+		-- Clear the screen
+		vim.cmd("redraw")
+
+		-- Print the countdown value
+		print("Countdown: " .. i)
+
+		-- Wait for 1 second
+		vim.cmd("sleep 1000m")
+	end
+
+	-- Open the floating terminal
+	open_float_terminal()
 end
 
--- Function to be called from NeoVim command-line
-function M.setup()
-	vim.cmd([[
-    command! -nargs=1 Countdown lua require('countdown').countdown(<args>)
-  ]])
-end
-
-return M
+-- Define the custom command
+vim.cmd([[command! -nargs=1 Countdown lua start_countdown(<args>)]])
