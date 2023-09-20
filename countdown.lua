3c9dcfbcb08b585aa28e17a234db93e06cbeb173
xuanan92 <nguyentruongxuananhn@gmail.com>
2023-09-20 22:12:37 +0700

    update all

--- countdown.lua
+++ countdown.lua
@@ -1,26 +1,36 @@
 -- countdown.lua
 
 local M = {}
+local countdown_job_id
 
-function M.setup(config)
+function M.setup()
 	-- Setup code for the plugin (if needed)
 end
 
 function M.countdown(duration)
+	if countdown_job_id then
+		print("Countdown is already running. Please wait for the current countdown to finish.")
+		return
+	end
+
 	local timer = duration
 	local minutes, seconds
 
-	while timer >= 0 do
-		minutes = string.format("%02d", math.floor(timer / 60))
-		seconds = string.format("%02d", timer % 60)
+	countdown_job_id = vim.fn.jobstart({
+		"sh",
+		"-c",
+		string.format([[sleep %ds && echo 'Countdown: Time is up!']], duration),
+	})
 
-		vim.cmd("echo 'Countdown: " .. minutes .. ":" .. seconds .. "'")
-		vim.cmd("sleep 1000m")
+	vim.fn.chansend(countdown_job_id, "exit\n")
 
-		timer = timer - 1
-	end
-
-	vim.cmd("echo 'Countdown: Time is up!'")
+	vim.fn.jobwait([[
+    function(job_id, data, event)
+      if event == "exit" then
+        vim.api.nvim_feedkeys(":echo 'Countdown: Time is up!'", "n", true)
+      end
+    endfunction
+  ]])
 end
 
-return M
+return M -- countdown.lua
