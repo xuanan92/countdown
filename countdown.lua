83e139aa5f6b2e60a866ca1ce54d738dbd910042
xuanan92 <nguyentruongxuananhn@gmail.com>
2023-09-20 23:26:53 +0700

    u

--- countdown.lua
+++ countdown.lua
@@ -23,9 +23,9 @@ function M.countdown(seconds)
     done
     echo 'Countdown: Time is up!']],
 	}, {
-		on_stdout = function(_, data)
+		on_stdout = function(_, _, data)
 			local output = table.concat(data, "\n")
-			notify(output:gsub('"', '\\"'), { title = "Countdown", timeout = 0 })
+			notify(tostring(output), { title = "Countdown", timeout = 0 })
 		end,
 		on_exit = function()
 			countdown_job_id = nil
