919b767c2a9264e8989cacd6263c9ce4da936e8b
xuanan92 <nguyentruongxuananhn@gmail.com>
2023-09-20 21:56:45 +0700

    update

--- countdown.lua
+++ countdown.lua
@@ -1,9 +1,26 @@
-print("Hello World!")
+-- countdown.lua
 
-local function some_function()
-	print("yeah, it works")
+local M = {}
+
+function M.setup(config)
+	-- Setup code for the plugin (if needed)
 end
 
-return {
-	some_function = some_function,
-}
+function M.countdown(duration)
+	local timer = duration
+	local minutes, seconds
+
+	while timer >= 0 do
+		minutes = string.format("%02d", math.floor(timer / 60))
+		seconds = string.format("%02d", timer % 60)
+
+		vim.cmd("echo 'Countdown: " .. minutes .. ":" .. seconds .. "'")
+		vim.cmd("sleep 1000m")
+
+		timer = timer - 1
+	end
+
+	vim.cmd("echo 'Countdown: Time is up!'")
+end
+
+return M
