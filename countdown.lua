87ca87cbe6eafda0ce393bedf33bea9f5f5a2235
xuanan92 <nguyentruongxuananhn@gmail.com>
2023-09-20 23:31:07 +0700

    u

--- countdown.lua
+++ countdown.lua
@@ -2,36 +2,35 @@
 
 local notify = require("notify")
 local M = {}
-local countdown_job_id
-
-function M.setup()
-	-- Setup code for the plugin (if needed)
-end
 
 function M.countdown(seconds)
-	if countdown_job_id then
-		print("Countdown is already running. Please wait for the current countdown to finish.")
-		return
+	local remaining = seconds
+
+	local function update_notification()
+		notify("Countdown: " .. tostring(remaining) .. " seconds remaining", {
+			title = "Countdown",
+			timeout = 0,
+		})
 	end
 
-	countdown_job_id = vim.fn.jobstart({
-		"sh",
-		"-c",
-		[[for ((s=]] .. seconds .. [[; s>=0; s--)); do
-        printf "Countdown: %02d seconds remaining\n" $s
-        sleep 1
-    done
-    echo 'Countdown: Time is up!']],
-	}, {
-		on_stdout = function(_, data)
-			local output = table.concat(data, "\n")
-			notify(output, { title = "Countdown", timeout = 0 })
-		end,
-		on_exit = function()
-			countdown_job_id = nil
-			notify("Time is up!", { title = "Countdown", timeout = 0 })
-		end,
-	})
+	local function finish_notification()
+		notify("Countdown: Time is up!", {
+			title = "Countdown",
+			timeout = 0,
+		})
+	end
+
+	local function countdown_loop()
+		if remaining > 0 then
+			update_notification()
+			remaining = remaining - 1
+			vim.defer_fn(countdown_loop, 1000) -- Schedule the next iteration after 1 second
+		else
+			finish_notification()
+		end
+	end
+
+	countdown_loop()
 end
 
 -- Function to be called from Neovim command-line
