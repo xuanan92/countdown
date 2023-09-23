a994dbc333a73bb92d73d4827eec2f4e85a36d5a
xuanan92 <nguyentruongxuananhn@gmail.com>
2023-09-23 13:52:09 +0700

    u

--- lua/countdown/init.lua
+++ lua/countdown/init.lua
@@ -55,10 +55,10 @@ function M.countdown(duration)
 			-- Append spent duration on the first line of the current editor
 			local current_lines = vim.api.nvim_buf_get_lines(current_buffer, 0, 1, false)
 			local duration_line = current_lines[1] or ""
-			local duration_spent = string.match(duration_line, "#(%d+(%.%d+)?)#")
-			-- local duration_spent = string.match(duration_line, "#([^#]+)#")
+			-- local duration_spent = string.match(duration_line, "#(%d+(%.%d+)?)#")
+			local duration_spent = string.match(duration_line, "#([^#]+)#")
 			local durationToMin = duration / 60
-			local roundedDurationToMin = string.format("%0.2f", durationToMin)
+			local roundedDurationToMin = string.format("%0.0f", durationToMin)
 			local new_duration
 
 			if duration_spent then
