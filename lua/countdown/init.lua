188bc677a7b2da8fbc510230e0324c1e5d002aab
xuanan92 <nguyentruongxuananhn@gmail.com>
2023-09-23 14:17:44 +0700

    u

--- lua/countdown/init.lua
+++ lua/countdown/init.lua
@@ -56,7 +56,7 @@ function M.countdown(duration)
 			local current_lines = vim.api.nvim_buf_get_lines(current_buffer, 0, 1, false)
 			local duration_line = current_lines[1] or ""
 			local duration_spent = string.match(duration_line, "#([^#]+)#")
-			local durationToMin = string.format("%d", math.floor(duration_spent / 60))
+			local durationToMin = math.floor(duration_spent / 60)
 			local new_duration
 
 			if duration_spent then
