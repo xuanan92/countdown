ca4b26bc602dd71ca091f47210ba0cfe2050940a
xuanan92 <nguyentruongxuananhn@gmail.com>
2023-11-25 09:33:59 -0500

    update

--- lua/countdown/init.lua
+++ lua/countdown/init.lua
@@ -9,7 +9,7 @@ end
 
 function M.sound()
 	-- linux
-	os.execute("timeout 1 aplay " .. os.getenv("HOME") .. "/.local/nvim/lazy/countdown/media/pr_in_space.mp3")
+	os.execute("timeout 1 aplay " .. os.getenv("HOME") .. "/.local/share/nvim/lazy/countdown/media/pr_in_space.mp3")
 end
 
 function M.countdown(minus)
