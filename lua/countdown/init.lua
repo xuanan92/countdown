4857e17e8f4ce9083571b4429c46f8be38d20532
xuanan92 <nguyentruongxuananhn@gmail.com>
2023-11-25 09:18:19 -0500

    update

--- lua/countdown/init.lua
+++ lua/countdown/init.lua
@@ -9,7 +9,7 @@ end
 
 function M.sound()
 	-- linux
-	local audioPath = os.getenv("HOME") .. "/.local/share/nvim/lazy/countdown/media/pr_in_space.mp3"
+	local audioPath = "~/.local/share/nvim/lazy/countdown/media/pr_in_space.mp3"
 	os.execute("timeout 1 aplay" .. audioPath)
 end
 
